c613328b9177dfc23d828c5993e7d331930deebe
Stevenson Nelson <sonson521@gmail.com>
2021-12-11 14:24:10 -0500

    add class constructor

--- app.js
+++ app.js
@@ -4,7 +4,24 @@ let formEmail = document.getElementById('formEmail');
 let formPassword = document.getElementById('formPassword');
 let submitButton = document.getElementById('submitButton');
 
+class Information {
+  constructor(name, email, password) {
+    this.name = name;
+    this.email = email;
+    this.password = password;
+  }
+  greeting() {
+    return `Welcome ${formName.value}`;
+  }
+}
+// Add Event Listener
 form.addEventListener('submit', function (e) {
   e.preventDefault();
-  console.log('submit');
+  const user1 = new Information(
+    formName.value,
+    formEmail.value,
+    formPassword.value
+  );
+
+  console.log(user1.greeting());
 });
