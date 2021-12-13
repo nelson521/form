da9de7ec2c3ede8decae4a7bc9820ce1250c16fc
Stevenson Nelson <sonson521@gmail.com>
2021-12-13 14:55:38 -0500

    change to user name

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let form = document.getElementById('form');
-let formName = document.getElementById('formName');
+let userName = document.getElementById('formName');
 let formEmail = document.getElementById('formEmail');
 let formPassword = document.getElementById('formPassword');
 let submitButton = document.getElementById('submitButton');
@@ -11,14 +11,14 @@ class Information {
     this.password = password;
   }
   greeting() {
-    return `Welcome ${formName.value}`;
+    return `Welcome ${userName.value}`;
   }
 }
 // Add Event Listener
 form.addEventListener('submit', function (e) {
   e.preventDefault();
   const user1 = new Information(
-    formName.value,
+    userName.value,
     formEmail.value,
     formPassword.value
   );
